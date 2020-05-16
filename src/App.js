f96c6e9ff08cb1da362c78321a57a186a8ddc99f
Tim Seifert <timothy.seifert@tufts.edu>
2020-05-16 13:57:37 -0400

    added slash

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 
 import React, { Component } from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import { Home } from './Home';
 import { About } from './About';
 import { Download } from './Resumes'
--- src/Components/NavigationBar.js
+++ src/Components/NavigationBar.js
@@ -31,9 +31,9 @@ export const NavigationBar = () => (
         <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="navbar">
                 <Nav.Item><Nav.Link href="/">Home</Nav.Link></Nav.Item>
-                <Nav.Item><Nav.Link href="/about">About</Nav.Link></Nav.Item>
-                <Nav.Item><Nav.Link href="/contact">Contact</Nav.Link></Nav.Item>
-                <Nav.Item><Nav.Link href="/resume">Resume</Nav.Link></Nav.Item>
+                <Nav.Item><Nav.Link href="/#/about">About</Nav.Link></Nav.Item>
+                <Nav.Item><Nav.Link href="/#/contact">Contact</Nav.Link></Nav.Item>
+                <Nav.Item><Nav.Link href="/#/resume">Resume</Nav.Link></Nav.Item>
             </Nav>            
     </Navbar.Collapse>
     </Navbar>
