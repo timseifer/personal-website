eec9bdc7380a097e3062278e40f2bf37ccbd48c5
Tim Seifert <timothy.seifert@tufts.edu>
2020-05-16 14:21:43 -0400

    fixed home

--- src/Components/NavigationBar.js
+++ src/Components/NavigationBar.js
@@ -26,7 +26,7 @@ const Styles = styled.div`
 export const NavigationBar = () => (
     <Styles>
     <Navbar expand="lg">
-        <Navbar.Brand href="/">Tim Seifert</Navbar.Brand>
+        <Navbar.Brand href="/personal-website/#/">Tim Seifert</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="navbar">
