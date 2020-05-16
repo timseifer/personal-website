17d01510aad2e6781807a1ffc367e9f0e0853298
Tim Seifert <timothy.seifert@tufts.edu>
2020-05-16 14:19:47 -0400

    fixed home

--- src/Components/NavigationBar.js
+++ src/Components/NavigationBar.js
@@ -30,10 +30,10 @@ export const NavigationBar = () => (
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="navbar">
-                <Nav.Item><Nav.Link href="/#/">Home</Nav.Link></Nav.Item>
-                <Nav.Item><Nav.Link href="/#/about/">About</Nav.Link></Nav.Item>
-                <Nav.Item><Nav.Link href="/#/contact/">Contact</Nav.Link></Nav.Item>
-                <Nav.Item><Nav.Link href="/#/resume/">Resume</Nav.Link></Nav.Item>
+                <Nav.Item><Nav.Link href="/personal-website/#/">Home</Nav.Link></Nav.Item>
+                <Nav.Item><Nav.Link href="/personal-website/#/about/">About</Nav.Link></Nav.Item>
+                <Nav.Item><Nav.Link href="/personal-website/#/contact/">Contact</Nav.Link></Nav.Item>
+                <Nav.Item><Nav.Link href="/personal-website/#/resume/">Resume</Nav.Link></Nav.Item>
             </Nav>            
     </Navbar.Collapse>
     </Navbar>
