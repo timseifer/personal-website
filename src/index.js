bcaeaf743be2fcfc9928d1815d8392df3fd93e88
Tim Seifert <timothy.seifert@tufts.edu>
2020-08-12 16:20:23 -0400

    fixing small error with website name

--- src/index.js
+++ src/index.js
@@ -6,9 +6,7 @@ import * as serviceWorker from './serviceWorker';
 import { HashRouter } from "react-router-dom";
 
 ReactDOM.render(
-  <HashRouter>
-    <App />
-  </HashRouter>,
+  <HashRouter><App /></HashRouter>,
   document.getElementById('root')
 );
 
